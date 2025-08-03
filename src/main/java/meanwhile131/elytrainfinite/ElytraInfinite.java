48463220ba13810d8309eab20e65b25bfc8a4601
meanwhile131 <51206659+meanwhile131@users.noreply.github.com>
2025-08-03 15:06:11 +0300

    Add basic pitch controls

--- src/main/java/meanwhile131/elytrainfinite/ElytraInfinite.java
+++ src/main/java/meanwhile131/elytrainfinite/ElytraInfinite.java
@@ -4,22 +4,54 @@ import net.fabricmc.api.ClientModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.Heightmap;
+
+enum FlyState {
+	NOT_FLYING,
+	PITCHING_DOWN,
+	GLIDING_DOWN
+};
 
 public class ElytraInfinite implements ClientModInitializer {
 	public static final String MOD_ID = "elytra-autopilot";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
+	
+	static final float pitchDown = 34f;
+	static final float pitchUp = -47f;
+	static final float pitchDownSpeed = 0.5f;
+	private FlyState state = FlyState.NOT_FLYING;
+	
 	@Override
 	public void onInitializeClient() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ClientTickEvents.START_WORLD_TICK.register(world -> {
+			ClientPlayerEntity player = MinecraftClient.getInstance().player;
+			if (player == null || !player.isGliding())
+				return;
+			if (state == FlyState.NOT_FLYING) {
+				state = FlyState.PITCHING_DOWN;
+			}
+			if (state == FlyState.PITCHING_DOWN) {
+				float pitch = player.getPitch();
+				pitch += Math.min(pitchDown - pitch, pitchDownSpeed); // change pitch by no more than pitchDownSpeed
+				if (pitch >= pitchDown) // fully pitched down, start gliding
+					state = FlyState.GLIDING_DOWN;
+				player.setPitch(pitch);
+			}
+			if (state == FlyState.GLIDING_DOWN) {
+				BlockPos pos = player.getBlockPos();
+				int height = world.getChunk(pos).sampleHeightmap(Heightmap.Type.WORLD_SURFACE, pos.getX(), pos.getZ());
+				if (pos.getY() - height < 5) {
+					player.setPitch(pitchUp);
+					state = FlyState.PITCHING_DOWN;
+				}
+				else
+					player.setPitch(pitchDown); // enforce down pitch
+			}
+		});
+		LOGGER.info("Elytra infinite loaded.");
 	}
 }

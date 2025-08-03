853fb8a3ec9b44c4e24b4bae7ba5fe0b7065910f
meanwhile131 <51206659+meanwhile131@users.noreply.github.com>
2025-08-03 16:39:11 +0300

    Add toggle keybind

--- src/main/java/meanwhile131/elytrainfinite/ElytraInfinite.java
+++ src/main/java/meanwhile131/elytrainfinite/ElytraInfinite.java
@@ -2,15 +2,24 @@ package meanwhile131.elytrainfinite;
 
 import net.fabricmc.api.ClientModInitializer;
 
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.option.StickyKeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Heightmap;
 
 enum FlyState {
+	TOGGLED_OFF,
 	NOT_FLYING,
 	PITCHING_DOWN,
 	GLIDING_DOWN
@@ -19,18 +28,27 @@ enum FlyState {
 public class ElytraInfinite implements ClientModInitializer {
 	public static final String MOD_ID = "elytra-autopilot";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	
+
 	static final float pitchDown = 34f;
 	static final float pitchUp = -47f;
 	static final float pitchDownSpeed = 0.5f;
 	private FlyState state = FlyState.NOT_FLYING;
-	
+	private static KeyBinding toggleKeybind;
+
 	@Override
 	public void onInitializeClient() {
+		toggleKeybind = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+			"key.elytrainfinite.toggle",
+			GLFW.GLFW_KEY_H,
+			"category.elytrainfinite"
+		));
 		ClientTickEvents.START_WORLD_TICK.register(world -> {
+			if (state == FlyState.TOGGLED_OFF) return;
 			ClientPlayerEntity player = MinecraftClient.getInstance().player;
-			if (player == null || !player.isGliding())
+			if (player == null || !player.isGliding()) {
+				state = FlyState.NOT_FLYING;
 				return;
+			}
 			if (state == FlyState.NOT_FLYING) {
 				state = FlyState.PITCHING_DOWN;
 			}
@@ -47,9 +65,22 @@ public class ElytraInfinite implements ClientModInitializer {
 				if (pos.getY() - height < 5) {
 					player.setPitch(pitchUp);
 					state = FlyState.PITCHING_DOWN;
-				}
-				else
+				} else
 					player.setPitch(pitchDown); // enforce down pitch
+			}
+		});
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			while (toggleKeybind.wasPressed()) {
+				MutableText msg = Text.literal("Infinite Elytra ");
+				if (state == FlyState.TOGGLED_OFF) {
+					state = FlyState.NOT_FLYING;
+					msg.append(Text.literal("ON").formatted(Formatting.GREEN));
+				}
+				else {
+					state = FlyState.TOGGLED_OFF;
+					msg.append(Text.literal("OFF").formatted(Formatting.RED));
+				}
+				client.player.sendMessage(msg, true);
 			}
 		});
 		LOGGER.info("Elytra infinite loaded.");
